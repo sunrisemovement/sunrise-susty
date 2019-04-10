03d67a7b1f572aa2de18df9199ba407da48b489a
Luke Westby <hello@lukewestby.com>
2019-04-10 15:10:14 -0700

    preload font files

--- header.php
+++ header.php
@@ -13,7 +13,16 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
+    <link rel="preload"
+          as="font"
+          type="font/woff2"
+          href="<?php echo get_theme_root_uri() . '/' . get_template() . '/fonts/source-sans-pro-v12-latin-regular.woff2' ?>"
+          crossorigin>
+    <link rel="preload"
+          as="font"
+          type="font/woff2"
+          href="<?php echo get_theme_root_uri() . '/' . get_template() . '/fonts/source-sans-pro-v12-latin-700.woff2' ?>"
+          crossorigin>
 	<?php wp_head(); ?>
 </head>
 
